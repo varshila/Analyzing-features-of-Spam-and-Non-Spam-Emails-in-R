886dec7b73c0c4a109730f3f92aa8b93f7d47926
Varshila Redkar <38543852+varshila@users.noreply.github.com>
2019-06-09 18:34:20 -0400

    Add files via upload

---
+++ PredictingIfAnEmailIsSpam.R
@@ -0,0 +1,55 @@
+# Load packages
+library(ggplot2)
+library(dplyr)
+library(openintro)
+
+# Compute summary statistics
+email %>%
+  group_by(spam) %>%
+  summarize(median(num_char) , IQR(num_char))
+
+# Create plot
+email %>%
+  mutate(log_num_char = log(num_char)) %>%
+  ggplot(aes(x = spam, y = log_num_char)) +
+  geom_boxplot()
+  
+# Compute center and spread for exclaim_mess by spam
+email %>%
+ group_by(spam) %>%
+ summarize(median(exclaim_mess) , IQR(exclaim_mess))
+
+# Create plot for spam and exclaim_mess
+ggplot(email, aes(x= log(exclaim_mess+ 0.1))) +
+geom_histogram(position = "dodge") +
+facet_wrap(~spam)
+
+# Create plot of proportion of spam by image
+email %>%
+  mutate(has_image = image > 0) %>%
+  ggplot(aes(x = has_image, fill = spam)) +
+  geom_bar(position = "fill")
+  
+# Test if images count as attachments
+sum(email$attach < email$image)
+
+# Question 1
+email %>%
+  filter(dollar > 0) %>%
+  group_by(spam) %>%
+  summarize(median(dollar))
+
+# Question 2
+email %>%
+  filter(dollar > 10) %>%
+  ggplot(aes(x = spam)) +
+  geom_bar(position = "dodge")
+
+# Reorder levels
+email$number <- factor(email$number, levels = c("none","small", "big"))
+
+# Construct plot of number
+ggplot(email, aes(x = number)) +
+  geom_bar() +
+  facet_wrap(~spam)
+  
